e373000bcf277be539b8d79630dd2ec6f76b9702
kmjang <kmjang@infracube.co.kr>
2023-02-22 18:01:08 +0900

    feat:코테

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,12 +1,13 @@
 import React, { useRef, useState, useEffect } from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
-const list = [
-  { id: 0, name: "1번" },
-  { id: 1, name: "2번" },
-  { id: 2, name: "3번" },
-  { id: 3, name: "4번" },
-];
 const Test = () => {
+  const [list, setList] = useState([
+    { id: 0, name: "1번" },
+    { id: 1, name: "2번" },
+    { id: 2, name: "3번" },
+    { id: 3, name: "4번" },
+  ]);
   const [dragAndDrop, setDragAndDrop] = useState({
     draggedFrom: null, //드래그 시작인덱스
     draggedTo: null, //드래그 변경될 인덱스
@@ -18,7 +19,6 @@ const Test = () => {
   const onDragStart = (e) => {
     e.currentTarget.style.opacity = "0.4";
     const initialPosition = parseInt(e.currentTarget.dataset.position); //초기 인덱스
-
     setDragAndDrop({
       ...dragAndDrop,
       draggedFrom: initialPosition, //초기인덱스
@@ -38,33 +38,149 @@ const Test = () => {
       // 드래그 시작, 끝 인덱스를 활용해 새로운 배열로 반환해줌
       ...remainingItems.slice(0, draggedTo),
       itemDragged,
-      // ...remainingItems.slice(draggedTo),
+      ...remainingItems.slice(draggedTo),
     ];
 
-    // console.log(draggedFrom, "첫지점");
-    // console.log(draggedTo, "도착지점");
-    console.log(newList, "배열");
-    // console.log(itemDragged, "배열찍은거");
-    // console.log(remainingItems);
+    if (draggedTo !== dragAndDrop.draggedTo) {
+      // 놓을 수 있는 영역이 변경 되면 객체를 변경해줌
+      setDragAndDrop({
+        ...dragAndDrop,
+        updatedOrder: newList,
+        draggedTo: draggedTo,
+      });
+    }
+  };
+  const onDrop = (e) => {
+    setList(dragAndDrop.updatedOrder);
+    setDragAndDrop({
+      ...dragAndDrop,
+      draggedFrom: null,
+      draggedTo: null,
+    });
+  };
+  const onDragEnter = (e) => {
+    //잡은 Item이 다른 Item이랑 겹쳤을 때 발생
+    e.currentTarget.classList.add("over");
+  };
+  const onDragLeave = (e) => {
+    //영역밖나간 드래그 엘리먼트
+    e.currentTarget.classList.remove("over");
+  };
+  const onDragEnd = (e) => {
+    console.log(e.currentTarget);
+    e.currentTarget.style.opacity = "1";
+    const listItens = document.querySelectorAll(".draggable");
+    listItens.forEach((item) => {
+      item.classList.remove("over");
+    });
   };
 
-  useEffect(() => {
-    console.log(dragAndDrop);
-  }, [dragAndDrop]);
+  const [todos, setTodo] = useState([
+    { id: "0", title: "0공부" },
+    { id: "1", title: "1헬스" },
+    { id: "2", title: "2독서" },
+    { id: "3", title: "3산책" },
+    { id: "4", title: "4요리" },
+  ]);
+  const handleChange = (result) => {
+    if (!result.destination) return;
+    const items = [...todos]; // 새배열담기
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+    setTodo(items);
+  };
+
+  // function solution(a: number, b: number) {
+  //   return ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"][new Date(2023, a - 1, b).getDay()];
+  // }
+  // solution(2, 22);
+
+  // function solution(s: string) {
+  //   console.log(s.substr(Math.ceil(s.length / 2) - 1, s.length % 2 === 0 ? 2 : 1));
+  // }
+  // solution("abcde");
+
+  // function solution() {
+  //   const a = [1, 1, 3, 3, 0, 1, 1];
+  //   console.log(a.filter((c, i) => c !== a[i + 1]));
+  // }
+
+  // function solution(arr: any, divisor: any) {
+  //   const answer = arr.filter((el: number) => el % divisor === 0);
+  //   return console.log(answer.length ? answer.sort((p: number, c: number) => p - c) : [-1]);
+  // }
+
+  // solution([10, 5, 44, 45, 85], 5);
+
+  // const solution = (num1, num2) => {
+  //   const newArr = [];
+  //   for (let i = 1; i <= num1; i++) {
+  //     newArr.push(i);
+  //   }
+  //   return newArr.filter((c, i) => c % 2 == 0).reduce((a, c) => a + c);
+  // };
+  // solution(4);
+
+  // const solution = (a) => {
+  //   const text = a.split("").reverse().join().replace(/,/g, "");
+  //   console.log(text);
+  // };
+  // solution("my_string");
+
+  const solution = (a) => {
+    console.log(a.filter((c, i) => c % 2));
+    console.log(a.filter((c, i) => c % 2 == 0));
+
+    // a.filter((c, i) => c % 2 == 0);
+    a.map((c) => {
+      return console.log(c % 2);
+    });
+  };
+  solution([1, 2, 3, 4, 5]);
 
   return (
     <>
       <div>
-        <ul>
+        <ul className="test">
           {list.map((c, index) => {
             return (
-              <li key={c.id} draggable onDragStart={onDragStart} data-position={index} onDragOver={onDragOver}>
+              <li
+                key={c.id}
+                className="draggable"
+                draggable={true}
+                data-position={index} //순번
+                onDragStart={onDragStart} //시작 인덱스
+                onDragOver={onDragOver} // 시작에서 부딫히는 인덱스 계속
+                onDrop={onDrop} //놓을때 리스트 변경
+                onDragEnter={onDragEnter} // 곂칠때 1번
+                onDragLeave={onDragLeave}
+                onDragEnd={onDragEnd}
+              >
                 {c.name}
               </li>
             );
           })}
         </ul>
       </div>
+
+      <DragDropContext onDragEnd={handleChange}>
+        <Droppable droppableId="todos">
+          {(provided) => (
+            <ul className="todos" {...provided.droppableProps} ref={provided.innerRef}>
+              {todos.map(({ id, title }, index) => (
+                <Draggable key={id} draggableId={id} index={index}>
+                  {(provided) => (
+                    <li ref={provided.innerRef} {...provided.dragHandleProps} {...provided.draggableProps} key={id}>
+                      <p>{title}</p>
+                    </li>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </ul>
+          )}
+        </Droppable>
+      </DragDropContext>
     </>
   );
 };
